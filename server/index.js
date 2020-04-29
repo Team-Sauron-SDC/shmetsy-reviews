f3dbb2699e3404598f020296cea5336d79927594
Imran Kheraj <ikheraj17@gmail.com>
2020-04-29 18:51:22 -0500

    installed express and bodyparser, basic server up and running

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log('THIS IS A TEST PULL REQUEST')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+const port = 3000;
+app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
