b2592e1bead4316b42dde8112c728decc73df2fe
Imran Kheraj <ikheraj17@gmail.com>
2020-04-29 19:41:39 -0500

    ignored env file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const env = require('dotenv');
+const db = require('mysql');
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({
@@ -9,4 +11,9 @@ app.use(bodyParser.urlencoded({
 const port = 3000;
 app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
 
-const connection = db.createConnection();
+const connection = db.createConnection({
+  host: env.DB_HOST,
+  user: env.DB_USER,
+
+
+});
