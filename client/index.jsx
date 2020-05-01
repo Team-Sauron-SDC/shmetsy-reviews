7cd30e0202e13f3ffcc55a8f208cc2a93d1057d4
Imran Kheraj <ikheraj17@gmail.com>
2020-04-30 21:05:28 -0500

    Added components for reviewList and reviewListEntry, successfully rendering to DOM

---
+++ client/index.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+    }
+  }
+
+  render() {
+    return (<div>
+      Hello World
+    </div>)
+  }
+
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,7 +7,7 @@ const db = require('mysql');
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 const port = 3000;
 app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
