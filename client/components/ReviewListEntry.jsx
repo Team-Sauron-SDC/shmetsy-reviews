51eec32c276bd3fc6966703ac8cffc42235f995e
Imran Kheraj <ikheraj17@gmail.com>
2020-05-05 19:20:39 -0500

    implemented user icon CSS class, text decoration for username, as well as hover highlight

--- client/components/ReviewListEntry.jsx
+++ client/components/ReviewListEntry.jsx
@@ -5,7 +5,7 @@ const ReviewListEntry = ({review}) =>
   (<div>
     <div className="entry-container">
     <img src="./user.jpg" className="user-icon"></img>
-    <div>{review.username}</div>
+    <div className="username">{review.username}</div>
     <div>{new Date(review.reviewDate).toDateString().substring(3,10) + ', ' + new Date(review.reviewDate).toDateString().substring(11)}</div>
     </div>
     <Rating emptySymbol="fa fa-star-o" fullSymbol="fa fa-star" initialRating={review.rating} readonly={true}/>
