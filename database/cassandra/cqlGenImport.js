e1d1f18458a3537c33085c51e871b9e37510bf66
carlitoswillis <carlitoswillis@berkeley.edu>
2020-05-29 18:46:45 -0700

    finish up queries and start stress testing

--- database/cassandra/cqlGenImport.js
+++ database/cassandra/cqlGenImport.js
@@ -1,24 +1,8 @@
 /* eslint-disable quotes */
 /* eslint-disable no-console */
+const uuid = require('uuid');
 const fs = require('fs');
 const faker = require('faker');
-const cassandra = require('cassandra-driver');
-
-const tempClient = new cassandra.Client({ contactPoints: ['db'], localDataCenter: 'datacenter1', keyspace: 'system' });
-const client = new cassandra.Client({ contactPoints: ['db'], localDataCenter: 'datacenter1', keyspace: 'sauron_sdc' });
-const connectAndCreate = () => tempClient.connect()
-  .then(() => {
-    const create = "CREATE KEYSPACE IF NOT EXISTS sauron_sdc WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : '1' }AND DURABLE_WRITES =  true;";
-    return tempClient.execute(create);
-  })
-  .then(() => {
-    client.connect((err) => (err ? console.log('There was an ERROR', err) : console.log('Connected to Cassandra!')));
-  })
-  .then(() => {
-    const createTable = 'CREATE TABLE IF NOT EXISTS sauron_sdc.reviews ((id int), username text, rating int, reviewdate text, review text, productid int, shopid int, PRIMARY KEY(id, productID, shopID))';
-    return client.execute(createTable);
-  })
-  .catch((err) => console.log('Cannot Connect to Cassandra!', err));
 
 const gen = () => {
   const username = faker.name.findName();
@@ -27,21 +11,21 @@ const gen = () => {
   const review = faker.lorem.sentences();
   // const productid = faker.random.number({ min: 1, max: 100 });
   const shopid = faker.random.number({ min: 1, max: 100000 });
+  const id = uuid.v1();
   const data = {
-    username, rating, reviewdate, review, shopid,
+    username, rating, reviewdate, review, shopid, id,
   };
-  // const params = [user, rating, reviewdate, review, productid, shopid];
   return data;
 };
 
 const start = new Date();
-const writeData = fs.createWriteStream('data.csv', {
+const writeData = fs.createWriteStream('data/data.csv', {
   flags: 'w',
 });
 writeData.write('username, rating, reviewdate, review, productid, shopid\n', 'utf8');
 
 const dataGen = (writer, encoding, callback) => {
-  let i = 10;
+  let i = 30000000;
   let productid = 1;
   function write() {
     let ok = true;
@@ -51,7 +35,7 @@ const dataGen = (writer, encoding, callback) => {
         productid += 1;
       }
       const entry = gen();
-      const data = `${entry.username}, ${entry.rating}, ${entry.reviewdate}, ${entry.review}, ${productid}, ${entry.shopid}\n`;
+      const data = `${entry.id}, ${entry.username}, ${entry.rating}, ${entry.reviewdate}, ${entry.review}, ${productid}, ${entry.shopid}\n`;
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
@@ -66,20 +50,8 @@ const dataGen = (writer, encoding, callback) => {
   write();
 };
 
-
-connectAndCreate()
-  .then(() => {
-    dataGen(writeData, 'utf-8', () => {
-      writeData.end();
-      client.execute(`COPY reviews(username, rating, reviewdate, review, productid, shopid) FROM '/data/data.csv' DELIMITER ',' CSV HEADER`, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('copying data into reviews');
-        }
-      });
-      const ending = new Date().getTime() - start.getTime();
-      console.log(`Seeding Complete! It took: ${Math.floor(ending / 60000)}m and ${((ending % 60000) / 1000).toFixed(0)}secs`);
-    });
-  })
-  .catch((err) => console.log('Connection or Seeding Error!', err));
+dataGen(writeData, 'utf-8', () => {
+  writeData.end();
+  const ending = new Date().getTime() - start.getTime();
+  console.log(`Seeding Complete! It took: ${Math.floor(ending / 60000)}m and ${((ending % 60000) / 1000).toFixed(0)}secs`);
+});
