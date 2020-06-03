1eaefa7e99486368d35ea5156d8bbc331d6d4316
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-03 08:44:09 -0700

    config bunyan

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const {
 
 const redis = new Redis();
 
-const log = bunyan.createLogger({ name: 'production', level: 'info' }).config.log();
+const log = () => bunyan.createLogger({ name: 'production', level: 'info' })();
 
 if (cluster.isMaster) {
   log('this is a master');
