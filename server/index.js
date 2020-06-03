3cc88521cb50830cf5a9379dd6aa697ad621f5c4
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-03 11:17:09 -0700

    rm logs

--- server/index.js
+++ server/index.js
@@ -13,145 +13,143 @@ const {
 const redis = new Redis();
 
 const log = bunyan.createLogger({ name: 'production' });
-log.info('Hello!');
+// log.info('Hello!');
 
 
-if (cluster.isMaster) {
-  log.info('this is a master');
-  cluster.fork();
-  cluster.fork();
-} else {
-  log.info('this is a worker');
-  const app = express();
-  app.use(compression());
-  app.use(cors());
-  app.use(express.static(path.join(__dirname, '/../public')));
-  app.use('/:id', express.static(`${__dirname}/../public`));
-  app.use(bodyParser.json());
+// if (cluster.isMaster) {
+// log.info('this is a master');
+//  cluster.fork();
+//  cluster.fork();
+// } else {
+// log.info('this is a worker');
+const app = express();
+app.use(compression());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use('/:id', express.static(`${__dirname}/../public`));
+app.use(bodyParser.json());
 
-  const port = 80;
-  app.listen(port, () => log.info(`App is listening at http://localhost:${port}`));
+const port = 80;
+app.listen(port, () => log.info(`App is listening at http://localhost:${port}`));
 
-  app.post('/api/reviews/', (req, res) => {
-    db.createReviews(req.body, (err, results) => {
-      if (err) {
-        res.status(404);
-        res.end();
-        throw err;
-      }
-      res.status(201);
+app.post('/api/reviews/', (req, res) => {
+  db.createReviews(req.body, (err, results) => {
+    if (err) {
+      res.status(404);
       res.end();
-    });
+      throw err;
+    }
+    res.status(201);
+    res.end();
   });
+});
 
-  const getShopReviews = (data, req, res) => {
-    const shop = data[0] ? data[0].shopid : 1;
-    db.readShopReviews(shop, (shopErr, results) => {
-      if (shopErr) {
-        res.status(404);
-        res.end();
-        log.info(shopErr);
-      } else {
-        redis.set(`shopid: ${shop}`, JSON.stringify(results))
-          .catch((e) => log.info(e));
-        const ids = new Set(data.map((review) => review.id));
-        let unsorted = new Set([...data]);
-        for (let i = 0; i < results.length; i += 1) {
-          if (!ids.has(results[i].id)) {
-            unsorted.add(results[i]);
-          }
+const getShopReviews = (data, req, res) => {
+  const shop = data[0] ? data[0].shopid : 1;
+  db.readShopReviews(shop, (shopErr, results) => {
+    if (shopErr) {
+      res.status(404);
+      res.end();
+      log.info(shopErr);
+    } else {
+      redis.set(`shopid: ${shop}`, JSON.stringify(results))
+        .catch((e) => log.info(e));
+      const ids = new Set(data.map((review) => review.id));
+      let unsorted = new Set([...data]);
+      for (let i = 0; i < results.length; i += 1) {
+        if (!ids.has(results[i].id)) {
+          unsorted.add(results[i]);
         }
-        unsorted = Array.from(unsorted);
-        const sorted = unsorted.sort((a, b) => b.reviewDate - a.reviewDate);
-        res.status(200).send(sorted);
       }
-    });
-  };
-  const getCachedShop = (data, req, res) => {
-    // Check the cache data from the server redis
-    const shopid = data[0] ? data[0].shopid : 1;
-    redis.get(`shopid: ${shopid}`, (err, result) => {
-      if (result) {
-        result = JSON.parse(result);
-        log.info('cached shop retrieved');
-        const ids = new Set(data.map((review) => review.id));
-        let unsorted = new Set([...data]);
-        for (let i = 0; i < result.length; i += 1) {
-          if (!ids.has(result[i].id)) {
-            unsorted.add(result[i]);
-          }
+      unsorted = Array.from(unsorted);
+      const sorted = unsorted.sort((a, b) => b.reviewDate - a.reviewDate);
+      res.status(200).send(sorted);
+    }
+  });
+};
+const getCachedShop = (data, req, res) => {
+  // Check the cache data from the server redis
+  const shopid = data[0] ? data[0].shopid : 1;
+  redis.get(`shopid: ${shopid}`, (err, result) => {
+    if (result) {
+      result = JSON.parse(result);
+      const ids = new Set(data.map((review) => review.id));
+      let unsorted = new Set([...data]);
+      for (let i = 0; i < result.length; i += 1) {
+        if (!ids.has(result[i].id)) {
+          unsorted.add(result[i]);
         }
-        unsorted = Array.from(unsorted);
-        const sorted = unsorted.sort((a, b) => b.reviewDate - a.reviewDate);
-        res.status(200).send(sorted);
-      } else {
-        getShopReviews(data, req, res);
       }
-    })
-      .catch((err) => {
-        getShopReviews(req, res);
-      });
-  };
-  const getProductReviews = (req, res) => {
-    const { id } = req.params;
-    db.readProductReviews(id, (err, data) => {
-      // data should be an array of reviews of product ${id}
-      if (err) {
-        res.status(404);
-        res.end();
-        log.info(err);
-      } else {
-        redis.set(`productid: ${id}`, JSON.stringify(data))
-          .catch((e) => log.info(e));
-        getCachedShop(data, req, res);
-      }
+      unsorted = Array.from(unsorted);
+      const sorted = unsorted.sort((a, b) => b.reviewDate - a.reviewDate);
+      res.status(200).send(sorted);
+    } else {
+      getShopReviews(data, req, res);
+    }
+  })
+    .catch((err) => {
+      getShopReviews(req, res);
     });
-  };
-
-
-  const getCachedProducts = (req, res) => {
-    // Check the cache data from the server redis
-    const { id } = req.params;
-    redis.get(`productid: ${id}`, (err, result) => {
-      if (result) {
-        result = JSON.parse(result);
-        log.info('cached products retrieved');
-        // res.send(result);
-        getCachedShop(result, req, res);
-      } else {
-        getProductReviews(req, res);
-      }
-    })
-      .catch((err) => {
-        getProductReviews(req, res);
-      });
-  };
-
-  app.get('/api/reviews/:id', getCachedProducts);
-
-  app.patch('/api/reviews/:id', (req, res) => {
-    const { id } = req.params;
-    db.updateReview({ entry: req.body, id }, (err, results) => {
-      if (err) {
-        log.info(err);
-        res.status(404);
-        res.end();
-      }
-      res.status(204);
+};
+const getProductReviews = (req, res) => {
+  const { id } = req.params;
+  db.readProductReviews(id, (err, data) => {
+    // data should be an array of reviews of product ${id}
+    if (err) {
+      res.status(404);
       res.end();
-    });
+      log.info(err);
+    } else {
+      redis.set(`productid: ${id}`, JSON.stringify(data))
+        .catch((e) => log.info(e));
+      getCachedShop(data, req, res);
+    }
   });
+};
 
-  app.delete('/api/reviews/:id', (req, res) => {
-    const { id } = req.params;
-    db.deleteReview(id, (err, results) => {
-      if (err) {
-        log.info(err);
-        res.status(404);
-        res.end();
-      }
-      res.status(204);
-      res.end();
+
+const getCachedProducts = (req, res) => {
+  // Check the cache data from the server redis
+  const { id } = req.params;
+  redis.get(`productid: ${id}`, (err, result) => {
+    if (result) {
+      result = JSON.parse(result);
+      // res.send(result);
+      getCachedShop(result, req, res);
+    } else {
+      getProductReviews(req, res);
+    }
+  })
+    .catch((err) => {
+      getProductReviews(req, res);
     });
+};
+
+app.get('/api/reviews/:id', getCachedProducts);
+
+app.patch('/api/reviews/:id', (req, res) => {
+  const { id } = req.params;
+  db.updateReview({ entry: req.body, id }, (err, results) => {
+    if (err) {
+      log.info(err);
+      res.status(404);
+      res.end();
+    }
+    res.status(204);
+    res.end();
   });
-}
+});
+
+app.delete('/api/reviews/:id', (req, res) => {
+  const { id } = req.params;
+  db.deleteReview(id, (err, results) => {
+    if (err) {
+      log.info(err);
+      res.status(404);
+      res.end();
+    }
+    res.status(204);
+    res.end();
+  });
+});
+//  }
