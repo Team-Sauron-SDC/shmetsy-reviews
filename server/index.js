caf1d15226fc3ae31c6f355c437c080e8684e0a0
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-03 08:37:28 -0700

    remove console.log

--- server/index.js
+++ server/index.js
@@ -5,18 +5,21 @@ require('newrelic');
 const Redis = require('ioredis');
 const cluster = require('cluster');
 const compression = require('compression');
+const bunyan = require('bunyan');
 const {
   env, express, path, bodyParser, cors, db,
 } = require('./imports');
 
 const redis = new Redis();
 
+const log = bunyan.createLogger({ name: 'production', level: 'info' }).config.log();
+
 if (cluster.isMaster) {
-  console.log('this is a master');
+  log('this is a master');
   cluster.fork();
   cluster.fork();
 } else {
-  console.log('this is a worker');
+  log('this is a worker');
   const app = express();
   app.use(compression());
   app.use(cors());
@@ -25,7 +28,7 @@ if (cluster.isMaster) {
   app.use(bodyParser.json());
 
   const port = 80;
-  app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
+  app.listen(port, () => log(`App is listening at http://localhost:${port}`));
 
   app.post('/api/reviews/', (req, res) => {
     db.createReviews(req.body, (err, results) => {
@@ -45,10 +48,10 @@ if (cluster.isMaster) {
       if (shopErr) {
         res.status(404);
         res.end();
-        console.log(shopErr);
+        log(shopErr);
       } else {
         redis.set(`shopid: ${shop}`, JSON.stringify(results))
-          .catch((e) => console.log(e));
+          .catch((e) => log(e));
         const ids = new Set(data.map((review) => review.id));
         let unsorted = new Set([...data]);
         for (let i = 0; i < results.length; i += 1) {
@@ -68,7 +71,7 @@ if (cluster.isMaster) {
     redis.get(`shopid: ${shopid}`, (err, result) => {
       if (result) {
         result = JSON.parse(result);
-        console.log('cached shop retrieved');
+        log('cached shop retrieved');
         const ids = new Set(data.map((review) => review.id));
         let unsorted = new Set([...data]);
         for (let i = 0; i < result.length; i += 1) {
@@ -94,10 +97,10 @@ if (cluster.isMaster) {
       if (err) {
         res.status(404);
         res.end();
-        console.log(err);
+        log(err);
       } else {
         redis.set(`productid: ${id}`, JSON.stringify(data))
-          .catch((e) => console.log(e));
+          .catch((e) => log(e));
         getCachedShop(data, req, res);
       }
     });
@@ -110,7 +113,7 @@ if (cluster.isMaster) {
     redis.get(`productid: ${id}`, (err, result) => {
       if (result) {
         result = JSON.parse(result);
-        console.log('cached products retrieved');
+        log('cached products retrieved');
         // res.send(result);
         getCachedShop(result, req, res);
       } else {
@@ -128,7 +131,7 @@ if (cluster.isMaster) {
     const { id } = req.params;
     db.updateReview({ entry: req.body, id }, (err, results) => {
       if (err) {
-        console.log(err);
+        log(err);
         res.status(404);
         res.end();
       }
@@ -141,7 +144,7 @@ if (cluster.isMaster) {
     const { id } = req.params;
     db.deleteReview(id, (err, results) => {
       if (err) {
-        console.log(err);
+        log(err);
         res.status(404);
         res.end();
       }
